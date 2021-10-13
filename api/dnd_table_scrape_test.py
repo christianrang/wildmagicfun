5f2e93bd748d1961bc425b23eed364b7cd981ffc
Christian Rang <44748386+misclikd@users.noreply.github.com>
2021-10-13 13:24:53 -0400

    Update dnd_table_scrape_test.py
    fixed broken test

--- api/dnd_table_scrape_test.py
+++ api/dnd_table_scrape_test.py
@@ -3,16 +3,16 @@ from dnd_table_scrape import match_d20_header
 
 class TestMatchD20Header(unittest.TestCase):
 
-    def test_all_possible_ints(self):
-        answer = ""
-        for i in range(1, 20):
-            if i <= 3:
-                answer = "1-3/Extreme"
-            if i <= 9:
-                answer = "4-9/Moderate"
-            if i <= 20:
-                answer = "10-20/Nuisance"
-            self.assertEqual(match_d20_header(i), answer)
+     def test_all_possible_ints(self):
+         answer = ""
+         for i in range(1, 20):
+             if i <= 3:
+                 answer = "1-3/Extreme"
+             if i > 3 and i <= 9:
+                 answer = "4-9/Moderate"
+             if i > 9 and i <= 20:
+                 answer = "10-20/Nuisance"
+             self.assertEqual(match_d20_header(i), answer)
 
 if __name__ == "__main__":
     unittest.main()
